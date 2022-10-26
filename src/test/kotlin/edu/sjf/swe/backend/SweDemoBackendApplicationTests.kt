5c1e30117ffaf1985c84d1577e4dfef919fa08ae
jacobvetere <jacobvetere@gmail.com>
2022-10-26 10:32:32 -0400

    Inital Commit

---
+++ src/test/kotlin/edu/sjf/swe/backend/SweDemoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.sjf.swe.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SweDemoBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ src/test/kotlin/edu/sjf/swe/backend/objects/EventTest.kt
@@ -0,0 +1,65 @@
+package edu.sjf.swe.backend.objects
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class EventTest {
+
+    @Test
+    fun isOver() {
+        val event = Event(0, 0, System.currentTimeMillis(), System.currentTimeMillis() * 2, "", "")
+        assertFalse(event.isOver())
+    }
+
+    @Test
+    fun testEquals() {
+        val event1 = Event(0, 0, System.currentTimeMillis(), System.currentTimeMillis() * 2, "", "")
+        val event2 = Event(0, 0, System.currentTimeMillis(), System.currentTimeMillis() * 2, "", "")
+        assertTrue(event1 == event2)
+
+        val event3 = Event(1, 0, System.currentTimeMillis(), System.currentTimeMillis() *1, "", "")
+        assertFalse(event1 == event3)
+        assertFalse(event2 == event3)
+    }
+
+    @Test
+    fun isEmpty() {
+        val event1 = Event(0, 0,0, 0, "", "")
+        assertFalse(event1.isEmpty())
+        val event2 = Event(-1, 0,0, 0, "", "")
+        assertTrue(event2.isEmpty())
+    }
+
+    @Test
+    fun testHashCode() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+
+    @Test
+    fun getEventId() {
+    }
+
+    @Test
+    fun getOrgID() {
+    }
+
+    @Test
+    fun getTimeStart() {
+    }
+
+    @Test
+    fun getTimeEnd() {
+    }
+
+    @Test
+    fun getLocation() {
+    }
+
+    @Test
+    fun getDescription() {
+    }
+}
